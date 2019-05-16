415fdf8eec6c731d0501882d5855b2ec78f058d0
danilopetrovic <danilo86petrovic@gmail.com>
2019-05-16 23:11:13 +0200

    novi xvm

--- res_mods/mods/xfw_packages/xvm_profiler/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_profiler/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
 __xvm_version__ = '7.9.2-dev'
 __wot_version__ = '1.5.0.2'
-__revision__ = '9067'
+__revision__ = '9068'
 __branch__ = 'default'
-__node__ = '2bd5767d54f45b3e2dca1799740946dad178694a'
+__node__ = '83c46571625c1dd46eeb60c914bb127362c67821'
