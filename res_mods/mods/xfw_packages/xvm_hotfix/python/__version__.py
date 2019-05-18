afcb111da5c2395ad93a01ee45a400baf9155db7
danilopetrovic <danilo86petrovic@gmail.com>
2019-05-18 13:48:47 +0200

    novi xvm

--- res_mods/mods/xfw_packages/xvm_hotfix/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_hotfix/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
 __xvm_version__ = '7.9.2-dev'
 __wot_version__ = '1.5.0.2'
-__revision__ = '9068'
+__revision__ = '9070'
 __branch__ = 'default'
-__node__ = '83c46571625c1dd46eeb60c914bb127362c67821'
+__node__ = '50c9a02eb2115108b54fab6481970f873a587b8d'
