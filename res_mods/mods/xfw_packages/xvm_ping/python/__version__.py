a583249ee3b345e23fec17a77dac9fda38a0e72d
danilopetrovic <danilo86petrovic@gmail.com>
2019-05-21 00:03:48 +0200

    nova verzija

--- res_mods/mods/xfw_packages/xvm_ping/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_ping/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
 __xvm_version__ = '7.9.3-dev'
 __wot_version__ = '1.5.0.3'
-__revision__ = '9081'
+__revision__ = '9091'
 __branch__ = 'default'
-__node__ = 'bd10ac80e86db79b27b22585af24293f562e8f2d'
+__node__ = '7131a306c0aef0e285170f08d2972cc2240bf83f'
