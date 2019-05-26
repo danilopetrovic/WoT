1f3d1f2ee6c0a2adda2615b08c6b6196addbb67b
danilopetrovic <danilo86petrovic@gmail.com>
2019-05-26 16:34:46 +0200

    valjda valja

--- res_mods/mods/xfw_packages/xvm_main/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_main/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
 __xvm_version__ = '7.9.3-dev'
-__wot_version__ = '1.5.0.3'
-__revision__ = '9091'
+__wot_version__ = '1.5.0.4'
+__revision__ = '9104'
 __branch__ = 'default'
-__node__ = '7131a306c0aef0e285170f08d2972cc2240bf83f'
+__node__ = 'cd9ae21d03a6a2d4cecacf95ac609aa8df0874fd'
