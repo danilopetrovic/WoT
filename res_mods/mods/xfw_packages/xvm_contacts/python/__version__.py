991dcfec15f109742cf670a055aa770cd3f01378
danilopetrovic <danilo86petrovic@gmail.com>
2019-05-29 01:29:39 +0200

    novi xvm i blacklista karabin_27

--- res_mods/mods/xfw_packages/xvm_contacts/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_contacts/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.3-dev'
+__xvm_version__ = '7.9.4-dev'
 __wot_version__ = '1.5.0.4'
-__revision__ = '9104'
+__revision__ = '9114'
 __branch__ = 'default'
-__node__ = 'cd9ae21d03a6a2d4cecacf95ac609aa8df0874fd'
+__node__ = '9426ef93773cda1d17a5cc63d95b8eea4d2bca15'
