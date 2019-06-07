4e0473af9759164e43330df8acc13ec0f1590c83
danilopetrovic <danilo86petrovic@gmail.com>
2019-06-07 07:58:42 +0200

    opet

--- res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_sounds/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.4-dev'
+__xvm_version__ = '7.9.4'
 __wot_version__ = '1.5.0.4'
-__revision__ = '9114'
-__branch__ = 'default'
-__node__ = '9426ef93773cda1d17a5cc63d95b8eea4d2bca15'
+__revision__ = '9133'
+__branch__ = 'wot-1.5.0'
+__node__ = 'a0ac8b206e942eb94d6b05b4b286b797a8ab541e'
