c81f7e0b97d51121581b5b800b7193cad8594c6f
danilopetrovic <danilo86petrovic@gmail.com>
2019-06-18 15:39:02 +0200

    nova verzija

--- res_mods/mods/xfw_packages/xvm_autologin/python/__version__.py
+++ res_mods/mods/xfw_packages/xvm_autologin/python/__version__.py
@@ -1,6 +1,6 @@
 # This file was created automatically from build script
-__xvm_version__ = '7.9.5-dev'
-__wot_version__ = '1.5.1.0'
-__revision__ = '9176'
+__xvm_version__ = '7.9.5'
+__wot_version__ = '1.5.1.1'
+__revision__ = '9179'
 __branch__ = 'default'
-__node__ = '063c37e566fc98f4b54a2a0aa8725053790409ce'
+__node__ = '4e10e02bf1fba50d807999bd32f98a4f536bebd3'
